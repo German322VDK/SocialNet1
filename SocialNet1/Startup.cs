a49f6cb9a03e457a5bfbe5a8867fc6eeaedf30d2
гера шлычков <germean322@gmail.com>
2022-03-08 22:04:01 +1000

    Создаём админку

--- SocialNet1/Startup.cs
+++ SocialNet1/Startup.cs
@@ -115,8 +115,14 @@ namespace SocialNet1
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                endpoints.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
 
                 endpoints.MapHub<MessageHub>("/chat");
             });
