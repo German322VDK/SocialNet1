d96d2c5b25bdf31e95c733991604f8b9ce820b75
гера шлычков <germean322@gmail.com>
2022-03-08 22:10:18 +1000

    Безопастность админки

--- SocialNet1/Areas/Admin/Controllers/HomeController.cs
+++ SocialNet1/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SocialNet1.Domain.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace SocialNet1.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = "Admin, God")]
     public class HomeController : Controller
     {
         public IActionResult Index()
