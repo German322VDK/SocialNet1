e5fd99125d2fd1014d574c7eb839c36c9a371afa
гера шлычков <germean322@gmail.com>
2022-03-20 21:02:44 +1000

    Статичные данные

--- SocialNet1/Models/Static/APIUrls.cs
+++ SocialNet1/Models/Static/APIUrls.cs
@@ -8,5 +8,7 @@ namespace SocialNet1.Models.Static
     public static class APIUrls
     {
         public const string ADD_IMAGE = "api/image/add";
+        public const string CONFIRM_EMAIL = "api/registr/confirm";
+        public const string GIVE_HASH = "api/registr/givehash";
     }
 }
---
+++ SocialNet1/Models/Static/Emails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNet1.Models.Static
+{
+    public static class Emails
+    {
+        public const string MAIN_EMAIL = "politspectr@gmail.com";
+
+        public const string MAIN_NAME = "politspectr";
+
+        public const string MAIN_PASS = "zxc322Abuser";
+    }
+}
