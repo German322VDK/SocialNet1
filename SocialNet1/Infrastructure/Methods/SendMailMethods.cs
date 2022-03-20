1ffc11135a641b2eae11df4992c4c3817beff8c9
гера шлычков <germean322@gmail.com>
2022-03-20 21:02:56 +1000

    методы для почты

---
+++ SocialNet1/Infrastructure/Methods/SendMailMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SocialNet1.Infrastructure.Methods
+{
+    public static class SendMailMethods
+    {
+        public static bool CheckEmail(string email) =>
+            new Regex(@"\b[A-Za-z][A-Za-z0-9]{2,15}[@][a-z]{4,10}[.][a-z]{2,4}\b").IsMatch(email);
+
+        public static async Task SendEmailAsync(string senderEmail, string SenderName, string senderPassword, string recicerEmail, string subj, string body)
+        {
+            var from = new MailAddress(senderEmail, SenderName);
+            var to = new MailAddress(recicerEmail);
+            var m = new MailMessage(from, to)
+            {
+                Subject = subj,
+                Body = $"{body} \n{DateTime.Now:f}" //<p></p>
+            };
+
+            var smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential(senderEmail, senderPassword);
+            smtp.EnableSsl = true;
+            await smtp.SendMailAsync(m);
+        }
+    }
+}
---
+++ SocialNet1/Infrastructure/Methods/StringGenerationMethods.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialNet1.Infrastructure.Methods
+{
+    public static class StringGenerationMethods
+    {
+        public static string Generate(int length, bool engLow = true, bool EngUp = false,
+            bool numbers = false, bool rusLow = false, bool rusUp = false)
+        {
+            var random = new Random();
+
+            var result = new StringBuilder(length);
+
+            string whiteList = "";
+
+            if (engLow)
+                whiteList = string.Concat(whiteList, FromTo('a', 'z').ToStr());
+
+            if (EngUp)
+                whiteList = string.Concat(whiteList, FromTo('A', 'Z').ToStr());
+
+            if (numbers)
+                whiteList = string.Concat(whiteList, FromTo('0', '9').ToStr());
+
+            if (rusLow)
+                whiteList = string.Concat(whiteList, FromTo('а', 'я').ToStr());
+
+            if (rusUp)
+                whiteList = string.Concat(whiteList, FromTo('А', 'Я').ToStr());
+
+            whiteList.ToArray();
+
+            for (int i = 0; i < length; i++)
+            {
+                var index = random.Next(0, whiteList.Length);
+
+                result.Append(whiteList[index]);
+            }
+
+            return result.ToString();
+        }
+
+        private static IEnumerable<char> FromTo(char start, char end)
+        {
+            if (start >= end)
+                throw new ArgumentOutOfRangeException();
+
+            for (int i = start; i < end; i++)
+            {
+                yield return (char)i;
+            }
+        }
+
+        private static string ToStr(this IEnumerable<char> chars) =>
+            new string(chars.ToArray());
+
+    }
+}
