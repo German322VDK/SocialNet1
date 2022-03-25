e2912b2fe84b2b9f7413cdedefdaf210c76ecbc9
гера шлычков <germean322@gmail.com>
2022-03-25 22:19:44 +1000

    Страница друзей

---
+++ SocialNet1/Controllers/FriendsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SocialNet1.Domain.Identity;
+using SocialNet1.Infrastructure.Interfaces.Based;
+using SocialNet1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNet1.Controllers
+{
+    public class FriendsController : Controller
+    {
+        private readonly IUser _user;
+        private readonly ILogger<FriendsController> _logger;
+
+        public FriendsController(IUser user, ILogger<FriendsController> logger)
+        {
+            _user = user;
+            _logger = logger;
+        }
+        public IActionResult Index(string username = null)
+        {
+            UserDTO user;
+
+            if (username is not null)
+            {
+                user = _user.Get(username);
+            }
+            else
+            {
+                user = _user.Get(User.Identity.Name);
+            }
+
+            _logger.LogInformation($"{User.Identity.Name} заходит к друзьям {user.UserName}");
+
+            var allFriends = user.Friends.Select(el => _user.Get(el.FriendName)).ToList();
+
+            var friends = allFriends
+                .Where(el => _user.IsFriend(el.UserName, user.UserName))
+                .ToList();
+
+            var subscriptions = allFriends
+                .Where(el => !_user.IsFriend(el.UserName, user.UserName))
+                .ToList();
+
+            var subscribers = _user.GetAll()
+                .Where(el => _user.IsFriend(el.UserName, user.UserName) && !_user.IsFriend(user.UserName, el.UserName))
+                .ToList();
+
+            return View(new FriendsViewModel 
+            { 
+                User = user,
+                Friends = friends,
+                Subscriptions = subscriptions,
+                Subscribers = subscribers
+            });
+        }
+    }
+}
