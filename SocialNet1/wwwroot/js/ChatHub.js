8e2dae3e5367086167cb8711f7a7483817cb7607
гера шлычков <germean322@gmail.com>
2022-04-10 14:40:37 +1000

    Да как это работает

--- SocialNet1/wwwroot/js/ChatHub.js
+++ SocialNet1/wwwroot/js/ChatHub.js
@@ -13,14 +13,6 @@
             let content = message.content;
             let date = message.date;
             let messageHelpId = message.messageHelpId;
-            // создаем элемент <b> для имени пользователя
-            //let userNameElem = document.createElement("b");
-            //userNameElem.appendChild(document.createTextNode(userName + ' : ' + date + ': '));
-
-            //// создает элемент <p> для сообщения пользователя
-            //let elem = document.createElement("p");
-            //elem.appendChild(userNameElem);
-            //elem.appendChild(document.createTextNode(content));
 
             //var lastElem = document.getElementById("chatroom").lastChild;
 
