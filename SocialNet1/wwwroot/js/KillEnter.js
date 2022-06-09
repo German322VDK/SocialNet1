1a8cc0bd55e63a6ed797bbadaaea859ad5381460
гера шлычков <germean322@gmail.com>
2022-06-09 20:41:59 +1000

    Запрещаем нажимать на Enter

---
+++ SocialNet1/wwwroot/js/KillEnter.js
@@ -0,0 +1,13 @@
+﻿////$(document).ready(function () {
+////    $(form).keydown(function (event) {
+////        if (event.keyCode == 13) {
+////            event.preventDefault();
+////            return false;
+////        }
+////    });
+////});
+
+document.getElementById('form').addEventListener('keydown', function (event) {
+    if (event.keyCode == 13)
+        event.preventDefault();
+});
